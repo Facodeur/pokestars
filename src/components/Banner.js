7caf26cae4bab0cb80150340efffa5b136ba8eb5
wilfried <wilfried.odorico@gmail.com>
2021-06-10 15:58:30 +0200

    creation banner + titre

---
+++ src/components/Banner.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+
+const Banner = ({ className }) => {
+  return (
+    <div className={className}>
+      <h1>PokéStars</h1>
+    </div>
+  )
+}
+
+export default styled(Banner)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ef5350;
+  border-bottom: 1px solid #fff;
+  border-top: 1px solid #fff;
+  box-shadow: 0px 0px 10px 10px rgba(0,0,0,0.5);
+  margin-top: 30px;
+`
