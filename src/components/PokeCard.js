bed70c16adb09bf2f37b405db801a546a48881a7
wilfried <wilfried.odorico@gmail.com>
2021-06-15 17:08:21 +0200

    ajout animation au survol des PokeCard

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -32,4 +32,10 @@ export default styled(PokeCard)`
   height: 180px;
   margin: 10px;
   box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 0.5);
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0px 0px 10px 5px rgba(255,255,255,0.57);
+    transform:scale(1.02);
+    transition: 0.3s;
+  }
 `
