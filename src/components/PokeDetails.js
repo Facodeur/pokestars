4f445e3838985a275e38a27ef4d718fbc9a43cbf
wilfried <wilfried.odorico@gmail.com>
2021-06-17 22:37:25 +0200

    ajout de l'image plus grande

--- src/components/PokeDetails.js
+++ src/components/PokeDetails.js
@@ -1,41 +1,49 @@
 import { useParams } from "react-router-dom";
-import { useEffect,useContext } from "react";
+import { useEffect, useContext } from "react";
 import PokeDataContext from "../services/context";
 import styled from "styled-components";
 import Spinner from "./Spinner";
 
 const PokeDetails = ({ className }) => {
-
   let { name } = useParams();
   const { getOnePoke, onePoke, isLoading } = useContext(PokeDataContext);
 
   useEffect(() => {
-    
     getOnePoke(name);
-    console.log(onePoke && onePoke)
+    console.log(onePoke && onePoke);
     // eslint-disable-next-line
   }, [name]);
 
-  if(isLoading) {
-    return <Spinner />
+  if (isLoading) {
+    return <Spinner />;
   }
 
+  // const stats = onePoke && onePoke.stats.map(stat => {
+  //    return stat.base_stat;
+  // })
 
-  
+  //console.log(stats)
+
   return (
-    <div className={className}>
-      
-      <img src={onePoke.sprites.front_default} alt="" />
-    </div>
+    <>
+      {onePoke && (
+        <div className={className}>
+          <h4>{onePoke.name}</h4>
+          <img src={onePoke.sprites.other.dream_world.front_default} alt="" />
+        </div>
+      )}
+    </>
   );
 };
 
 export default styled(PokeDetails)`
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
   margin-top: 50px;
   img {
-    width: 200px;
-    height: 200px;
+    width: 190px;
+    height: 190px;
   }
 `;
