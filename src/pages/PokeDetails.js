0ccf5b44cdad9e836b21694e8808571d8cafbbbe
wilfried <wilfried.odorico@gmail.com>
2021-07-08 11:40:03 +0200

    suppression du scale sur le hover

--- src/pages/PokeDetails.js
+++ src/pages/PokeDetails.js
@@ -62,7 +62,6 @@ export default styled(PokeDetails)`
     box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 0.5);
     &:hover {
       box-shadow: 0px 0px 6px 4px rgba(255, 255, 255, 0.57);
-      transform: scale(1.02);
       transition: 0.3s;
     }
   }
